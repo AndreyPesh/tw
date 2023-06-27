f336bf615faeb0e681989920842eb9730d5488b6
Andrey Peshko <coursestud@mail.ru>
2023-06-27 23:12:58 +0300

    feat: add style user button

--- src/3_features/userButton/UserButton.tsx
+++ src/3_features/userButton/UserButton.tsx
@@ -1,12 +1,15 @@
 'use client';
 import { MouseEvent } from 'react';
-import { signOut } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { EnumLinkPage } from '@/src/5_shared/types/enum';
 
 const UserButton = () => {
   const router = useRouter();
+  const { data: session } = useSession();
+  const userImage =
+    session && session.user?.image ? session.user.image : './avatar.svg';
 
   const onSignOut = (event: MouseEvent<HTMLElement>) => {
     event.stopPropagation();
@@ -16,13 +19,18 @@ const UserButton = () => {
   return (
     <div
       onClick={() => router.push(EnumLinkPage.USER)}
-      className="flex items-center cursor-pointer"
+      className="flex items-center justify-around cursor-pointer border-t"
     >
-      <Image priority alt="user" width={48} height={48} src={'./avatar.svg'} />
-      <p>
-        Username
-        <span></span>
-      </p>
+      <div className='p-2 inline-flex items-center hover:shadow-sm'>
+        <Image priority alt="user" width={48} height={48} src={userImage} />
+        <p className='ml-2 text-sm'>
+          {session && session.user ? (
+            <span>{session.user.name}</span>
+          ) : (
+            <span>Username</span>
+          )}
+        </p>
+      </div>
       <span
         onClick={onSignOut}
         className="ml-5 inline-flex w-[30px] h-[30px] bg-logout bg-no-repeat bg-center bg-color-white hover:bg-logout-hover"
