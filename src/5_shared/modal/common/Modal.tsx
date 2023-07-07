a5e235d15c4affbbf95843e4962cd8c6792278c4
Andrei <coursestud@mail.ru>
2023-07-07 21:32:31 +0300

    feat: add  modal handler

--- src/5_shared/modal/common/Modal.tsx
+++ src/5_shared/modal/common/Modal.tsx
@@ -1,11 +1,37 @@
+import classNames from 'classnames';
 import useAddImageModalStore from '../addImageModal/state';
+import { MouseEvent } from 'react';
+
+const enum MODAL_DATA_ATTR {
+  CLOSE = 'close',
+}
 
 const Modal = () => {
   const { isShow, closeModal } = useAddImageModalStore();
+
+  const toggle = (event: MouseEvent<HTMLElement>) => {
+    const { dataset } = event.target as HTMLElement;
+    if (dataset.modal === MODAL_DATA_ATTR.CLOSE) {
+      closeModal();
+    }
+    console.log(event);
+  };
+
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-slate-900/75">
-      <div className="w-full h-full flex items-center justify-center">
-        <div className="p-5 w-[80%] h-[80%] bg-white inset-0 rounded-lg">window</div>
+    <div
+      className={classNames(
+        'fixed top-0 left-0 w-full h-full bg-slate-900/75',
+        { hidden: !isShow }
+      )}
+      onClick={toggle}
+    >
+      <div
+        className="w-full h-full flex items-center justify-center"
+        data-modal={'close'}
+      >
+        <div className="p-5 w-[80%] h-[80%] bg-white inset-0 rounded-lg">
+          window
+        </div>
       </div>
     </div>
   );
