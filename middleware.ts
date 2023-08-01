e85fc71a56ada7c86c30b527d78e701b034deb1b
Andrey Peshko <coursestud@mail.ru>
2023-08-01 18:37:24 +0300

    refactor: test middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,12 @@
+import { NextRequest, NextResponse } from 'next/server';
+
 export { default } from 'next-auth/middleware';
 
-// export const config = { matcher: ['/user'] };
-export const config = { matcher: [] };
+export function middleware(request: NextRequest) {
+  console.log(`middleware ${request.nextUrl}`);
+  return NextResponse.next();
+  
+}
+
+export const config = { matcher: ['/user'] };
+// export const config = { matcher: [] };
