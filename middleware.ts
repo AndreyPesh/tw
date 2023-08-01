4712577fd1f547b9c45d982ad20ec54097334630
Andrey Peshko <coursestud@mail.ru>
2023-08-01 20:44:47 +0300

    refactor: test middleware

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,9 @@ import { withAuth } from 'next-auth/middleware';
 
 export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
-  // function middleware(req) {
-  //   console.log(req.nextauth.token);
-  // },
+  function middleware(req) {
+    console.log(req.nextauth);
+  },
   {
     secret: process.env.NEXTAUTH_SECRET as string,
   }
