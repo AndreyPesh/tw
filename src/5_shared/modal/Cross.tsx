67bcf583e6d639c059842232bad0ba1b451297bb
Andrey Peshko <coursestud@mail.ru>
2023-08-02 19:15:57 +0300

    feat: add styles to cross modal

--- src/5_shared/modal/Cross.tsx
+++ src/5_shared/modal/Cross.tsx
@@ -2,9 +2,13 @@ import { RxCross1 } from 'react-icons/rx';
 
 const Cross = () => {
   return (
-    <span className="absolute z-200 bg-transparent flex w-[25px] h-[25px] border top-5 right-5 cursor-pointer" data-modal="close">
-      <RxCross1 size={25} className='absolute z-[10]'/>
-    </span>
+    <>
+      <RxCross1 size={25} className="absolute top-5 right-5" />
+      <span
+        className="absolute bg-transparent block w-[25px] h-[25px] top-5 right-5 cursor-pointer"
+        data-modal="close"
+      ></span>
+    </>
   );
 };
 
