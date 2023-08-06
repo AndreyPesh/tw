d5f8f689b278c173d1da96c1275cb25860d242b7
Andrey Peshko <coursestud@mail.ru>
2023-08-06 18:38:14 +0300

    fix: fix

--- app/api/user/account/avatar/route.ts
+++ app/api/user/account/avatar/route.ts
@@ -11,11 +11,11 @@ const userDB = new UserDB();
 const setImageRequest = async (request: NextRequest) => {
   try {
     const session = await getServerSession();
-    console.log(session);
     
     if (!session) {
       return NextResponse.json({
         status: STATUS_CODE.UNAUTHORIZED,
+        data: session
       });
     }
     const { user } = session;
