1be74977dfe05327205ef423f377273ed7adf101
Andrey Peshko <coursestud@mail.ru>
2023-08-10 18:38:38 +0300

    fix: fix

--- app/api/user/account/email/route.ts
+++ app/api/user/account/email/route.ts
@@ -2,15 +2,14 @@ import { UserDB } from '@/src/5_shared/api/helpers/db/user/User';
 import { STATUS_CODE } from '@/src/5_shared/api/types/enums';
 import { NextRequest, NextResponse } from 'next/server';
 
-export const updateEmail = async (req: NextRequest) => {
+const updateEmail = async (req: NextRequest) => {
   try {
     const { email, newEmail }: { email: string; newEmail: string } =
       await req.json();
     const isEmailUpdated = await new UserDB().updateEmail(email, newEmail);
 
     if (isEmailUpdated) return NextResponse.json({ status: STATUS_CODE.OK });
-    // throw new Error('Cant update email');
-    return NextResponse.json({ status: STATUS_CODE.INTERNAL });
+    throw new Error('Cant update email');
   } catch (error) {
     return NextResponse.json({
       status: STATUS_CODE.INTERNAL,
@@ -19,4 +18,4 @@ export const updateEmail = async (req: NextRequest) => {
   }
 };
 
-export { updateEmail as POST };
+export { updateEmail as PATCH };
