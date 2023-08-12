bca2a53422fa464488a97779b7dfc10d44d1ed8b
Andrey Peshko <coursestud@mail.ru>
2023-08-12 16:55:31 +0300

    feat: add test category

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import { Suspense } from 'react';
 import Loading from './loading';
+import Main from '@/src/1_page/Main';
+
 
 const Home = async () => {
   return (
     <>
       <Suspense fallback={<Loading />}>
         <main className="container">main</main>
+        <Main />
       </Suspense>
     </>
   );
---
+++ src/1_page/Main.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+const Main = () => {
+  const add = () => {
+    fetch('/api/phone')
+      .then((res) => res.json())
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div>
+      <button onClick={add}>Add random category</button>
+    </div>
+  );
+};
+
+export default Main;
