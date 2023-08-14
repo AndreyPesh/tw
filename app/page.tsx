6dd07c13a08326c5e253fded7778fd7fe93c6ace
Andrey Peshko <coursestud@mail.ru>
2023-08-14 17:29:15 +0300

    refactor: refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import { Suspense } from 'react';
 import Loading from './loading';
-import Main from '@/src/1_page/Main';
-
 
 const Home = async () => {
   return (
     <>
       <Suspense fallback={<Loading />}>
         <main className="container">main</main>
-        <Main />
       </Suspense>
     </>
   );
--- src/5_shared/UI/buttons/AddToCartButton.tsx
+++ src/5_shared/UI/buttons/AddToCartButton.tsx
@@ -1,13 +1,15 @@
-import classNames from 'classnames';
-import { useState } from 'react';
+import { useState, MouseEvent } from 'react';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
+import classNames from 'classnames';
 
 const AddToCartButton = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [isInCart, setIsInCart] = useState(false);
 
-  const addToCartHandler = async () => {
-    console.log('add');
+  const addToCartHandler = async (event: MouseEvent<HTMLButtonElement>) => {
+    if ((event.target as HTMLButtonElement).dataset.cart) {
+      event.stopPropagation();
+    }
 
     try {
       setIsLoading(true);
@@ -27,12 +29,14 @@ const AddToCartButton = () => {
   return (
     <button
       className={classNames(
-        'p-2 w-[100px] inline-flex justify-between bg-red text-white text-sm rounded hover:shadow-bg transform active:scale-90 transition-transform disabled:scale-100 disabled:cursor-not-allowed',
+        'p-2 w-[100px] inline-flex justify-between text-white text-sm rounded hover:shadow-bg transform active:scale-90 transition-transform disabled:scale-100 disabled:cursor-not-allowed',
         { 'opacity-50': isLoading },
+        { 'bg-red': !isInCart },
         { 'bg-green-700': isInCart }
       )}
       onClick={addToCartHandler}
       disabled={isLoading}
+      data-cart="cart-button"
     >
       {<AiOutlineShoppingCart size={20} />} {isInCart ? 'Remove' : 'Add'}
     </button>
