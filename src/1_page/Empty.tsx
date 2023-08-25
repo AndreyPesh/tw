c78c4966420c9dfecbb83f89add669782743e037
Andrey Peshko <coursestud@mail.ru>
2023-08-25 17:54:19 +0300

    refactor: add image to empty component

--- src/1_page/Empty.tsx
+++ src/1_page/Empty.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 const Empty = () => {
   return (
-    <div className="w-full min-h-[100%] flex items-center justify-center">
-      No data
+    <div className="w-full min-h-[100%] flex flex-col items-center justify-center">
+      <h2>Data not found! Try it again...</h2>
+      <Image src={'/data_not_found.png'} width={500} height={500} alt="no data" />
     </div>
   );
 };
