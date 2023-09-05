253bc8767757a778a91773efe79ce4ffaaa2d138
Andrei <coursestud@mail.ru>
2023-09-05 15:04:57 +0300

    feat: add phone details table

--- src/3_features/phones/details/PhoneDetails.tsx
+++ src/3_features/phones/details/PhoneDetails.tsx
@@ -1,5 +1,5 @@
-import { PhoneDetails } from '@prisma/client';
 import { FC } from 'react';
+import { PhoneDetails } from '@prisma/client';
 
 interface PhoneDetailsProps {
   listDetails: PhoneDetails | null;
@@ -7,7 +7,36 @@ interface PhoneDetailsProps {
 
 const PhoneDetailsTable: FC<PhoneDetailsProps> = ({ listDetails }) => {
   return (
-    <div>{listDetails ? listDetails.display : 'Details are not found'}</div>
+    <div>
+      {listDetails ? (
+        <table>
+          <tbody>
+            <tr>
+              <th>Display:</th>
+              <td>{listDetails.display}</td>
+            </tr>
+            <tr>
+              <th>Mobile Operating System:</th>
+              <td>{listDetails.os}</td>
+            </tr>
+            <tr>
+              <th>Internal memory:</th>
+              <td>{listDetails.memory} Gb</td>
+            </tr>
+            <tr>
+              <th>Near Field Communication (NFC):</th>
+              <td>{listDetails.nfc ? <span>Yes</span> : <span>No</span>}</td>
+            </tr>
+            <tr>
+              <th>Type of charging cord:</th>
+              <td>{listDetails.charge}</td>
+            </tr>
+          </tbody>
+        </table>
+      ) : (
+        <h2>Details are not found</h2>
+      )}
+    </div>
   );
 };
 
