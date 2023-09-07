c7f0c687e90b51d38955556421bb6fa5d27a34f6
Andrei <coursestud@mail.ru>
2023-09-07 19:58:54 +0300

    refactor: add count to cart icon

--- src/3_features/cart/Cart.tsx
+++ src/3_features/cart/Cart.tsx
@@ -1,11 +1,29 @@
 import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { useCartQuery } from './hooks/useCartQuery';
+import { useEffect, useState } from 'react';
 
 const Cart = () => {
+  const [countCartItem, setCountCartItem] = useState(0);
+  const { listCart } = useCartQuery();
+
+  useEffect(() => {
+    const countItem = listCart?.length;
+    if (listCart && countItem) {
+      setCountCartItem(countItem);
+    }
+    
+  }, [listCart, listCart?.length]);
+
   return (
-    <div onClick={(e) => {e.stopPropagation()}} className="relative w-[30px] h-[65px] inline-flex items-center cursor-pointer active:scale-90 transition-all">
+    <div
+      onClick={(e) => {
+        e.stopPropagation();
+      }}
+      className="relative w-[30px] h-[65px] inline-flex items-center cursor-pointer active:scale-90 transition-all"
+    >
       <AiOutlineShoppingCart size={30} color="gray" />
       <span className="absolute top-2 right-[-12px] w-6 h-6 inline-flex items-center justify-center rounded-full text-white text-sm bg-red select-none">
-        10
+        {countCartItem}
       </span>
     </div>
   );
