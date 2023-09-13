cfeafeac0d4198dbcd31c62ef0315d1cd7a7d1f9
Andrei <coursestud@mail.ru>
2023-09-13 16:56:00 +0300

    fix: fix

--- src/3_features/userData/address/UI/Address.tsx
+++ src/3_features/userData/address/UI/Address.tsx
@@ -1,14 +1,11 @@
-import Button from '@/src/5_shared/buttons/Button';
-import { EnumTypeButton } from '@/src/5_shared/buttons/types/enums';
+import { FC } from 'react';
 import { Address } from '@prisma/client';
-import { Dispatch, FC, SetStateAction } from 'react';
 
 interface AddressDataProps {
   address: Address;
-  changeHandler: Dispatch<SetStateAction<boolean>>;
 }
 
-const AddressData: FC<AddressDataProps> = ({ address, changeHandler }) => {
+const AddressData: FC<AddressDataProps> = ({ address }) => {
   return (
     <div>
       <h3 className="py-1">Country: {address.country}</h3>
@@ -16,12 +13,6 @@ const AddressData: FC<AddressDataProps> = ({ address, changeHandler }) => {
       <h3 className="py-1">Street: {address.street}</h3>
       <h3 className="py-1">Building: {address.building}</h3>
       <h3 className="py-1">Post code: {address.postCode}</h3>
-      <Button
-        variant={EnumTypeButton.DANGER}
-        handler={() => changeHandler(true)}
-      >
-        Change address
-      </Button>
     </div>
   );
 };
