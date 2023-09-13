c9a704b7547b227a2aa824c828c0b4e2e4f98518
Andrei <coursestud@mail.ru>
2023-09-13 14:42:30 +0300

    feat: add styles to address delivery component

--- src/3_features/userData/address/AddressDelivery.tsx
+++ src/3_features/userData/address/AddressDelivery.tsx
@@ -1,9 +1,12 @@
+import { useEffect, useState } from 'react';
+import classNames from 'classnames';
 import { useQuery } from 'react-query';
 import AddressAPI from './fetch/AddressAPI';
 import { useSession } from 'next-auth/react';
 import AddressData from './UI/Address';
 
 const AddressDelivery = () => {
+  const [isShowEditAddressForm, setIsShowEditAddressForm] = useState(true);
   const session = useSession();
   const userId = session.data?.user.id ?? '';
 
@@ -19,9 +22,27 @@ const AddressDelivery = () => {
   }
 
   return (
-    <div className="p-2 mt-2 border rounded-lg shadow-lg">
-      <h2 className="font-bold">Address delivery:</h2>
-      {userAddress && <AddressData address={userAddress} />}
+    <div className="mt-2 w-full  text-sm border rounded-lg shadow-lg overflow-hidden">
+      <div
+        className={classNames('w-full flex transition-all', {
+          'translate-x-[-100%]': isShowEditAddressForm,
+        })}
+      >
+        <div className="p-2 w-full min-w-[100%]">
+          <h2 className="font-bold">Address delivery:</h2>
+          {userAddress && (
+            <AddressData
+              address={userAddress}
+              changeHandler={setIsShowEditAddressForm}
+            />
+          )}
+        </div>
+        <div className="p-2 w-full min-w-[100%]">
+          <h2 className="font-bold">Edit address delivery:</h2>
+          Edit form
+          <button onClick={() => setIsShowEditAddressForm(false)}>Data</button>
+        </div>
+      </div>
     </div>
   );
 };
