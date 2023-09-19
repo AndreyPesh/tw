ee90617b982bfbbda3414b41ec9d675c07a27b1d
Andrei <coursestud@mail.ru>
2023-09-19 18:48:27 +0300

    feat: add styles to user navigation

--- src/3_features/userData/navigation/UserNavigation.tsx
+++ src/3_features/userData/navigation/UserNavigation.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState, MouseEvent } from 'react';
 import NavItem from './ui/NavItem';
 import { navigationList } from './types/constants';
 
 const UserNavigation = () => {
+  const [activeItemDataset, setActiveItemDataset] = useState(
+    navigationList[0].title
+  );
+
+  const navigateHandler = (event: MouseEvent<HTMLDivElement>) => {
+    const node = event.target as HTMLDivElement;
+    const dataset = node.dataset.item;
+    if (dataset) {
+      setActiveItemDataset(dataset);
+    }
+  };
+
   return (
-    <nav>
+    <nav onClick={navigateHandler}>
       <ul>
         {navigationList.map((navItemData) => (
-          <NavItem {...navItemData} />
+          <NavItem
+            {...navItemData}
+            isActive={activeItemDataset === navItemData.title}
+          />
         ))}
       </ul>
     </nav>
--- src/3_features/userData/navigation/ui/NavItem.tsx
+++ src/3_features/userData/navigation/ui/NavItem.tsx
@@ -1,10 +1,18 @@
+import classNames from 'classnames';
 import { NavItemData } from '../types/interfaces';
 
-const NavItem = ({ title, icon: Icon }: NavItemData) => {
+interface NavItemProps extends NavItemData {
+  isActive: boolean;
+}
+
+const NavItem = ({ title, icon: Icon, isActive }: NavItemProps) => {
   return (
-    <li className="p-4 flex border border-transparent rounded-lg cursor-pointer select-none hover:border-slate-200">
+    <li
+      className={classNames("p-4 flex border border-transparent rounded-lg cursor-pointer select-none hover:border-slate-200", {'font-bold border-slate-300 shadow': isActive})}
+      data-item={title}
+    >
       <span className="mr-2">
-        <Icon size={25} color="gray" />
+        <Icon size={25} color={isActive ? "black" : "gray"} />
       </span>
       {title}
     </li>
